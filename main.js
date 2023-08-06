6d13edde1aef6f45de82fce679b9e71ede1e5c13
Genius4Hire <jordan.dennis@gmail.com>
2023-08-06 12:29:27 -0700

    Created demo for newly create card machanics

--- main.js
+++ main.js
@@ -2,8 +2,54 @@ const { Player, Card, Deck } = require("./src/game_class")
 
 const example = new Deck(["Magician","Archer","Sorcerer","Templar","Rogue"]);
 
-console.log("Original deck:", example.deck);
-console.log("");
-console.log("Drawing a card... ", example.pullCard());
-console.log("");
-console.log("Lost a card!: ",example.deck);
+
+const factions = ["Magician","Archer","Sorcerer","Templar","Rogue"];
+
+
+// set up piles of cards: Collections (as deck, player.hand and discard pile)
+const deck = new Deck(factions);
+const discard_pile = new Collection();
+
+// make some players
+const player1 = new Player("Fred", deck); 
+const player2 = new Player("Barney", deck);
+
+// setup loop condition
+let done = false;
+
+// run demo loop
+while (!done) {
+
+  // print out the state of each pile of cards, and some commands
+
+  console.clear();
+  console.log("Deck")
+  console.log(deck.getCollection());
+  console.log(player1.name);
+  console.log(player1.hand.getCollection());
+  console.log(player2.name);
+  console.log(player2.hand.getCollection());
+  console.log("Discard pile", discard_pile.getCollection());
+  console.log("");
+  const options = 
+    `q - quit\n`+
+    `b - give a random card from ${player1.name} -> ${player2.name}\n`+
+    `f - give a random card from ${player2.name} -> ${player1.name}\n`+
+    `1 - discard a card from ${player1.name}\n`+
+    `2 - discard a card from ${player2.name}\n`+
+    ``;
+  console.log(options);
+
+  // get a command from user
+  key = readlineSync.keyIn('Pick..');
+  
+  switch (key) {
+    case "q" : done = true; break;
+    case "b" : player2.hand.receiveCard(player1.hand.giveRandomCard()); break;
+    case "f" : player1.hand.receiveCard(player2.hand.giveRandomCard()); break;
+    case "1" : discard_pile.receiveCard(player1.hand.giveRandomCard()); break;
+    case "2" : discard_pile.receiveCard(player2.hand.giveRandomCard()); break;
+    default : break;
+  }
+}
+console.log("Quitting..");
--- src/game_class.js
+++ src/game_class.js
@@ -1,10 +1,94 @@
+// A player has a name, score and new hand drawn from
+// a passed-in deck
 class Player {
 
+  // takes a card and adds it to this player's hand
+  drawCard (deck) {
+    this.hand.receiveCard(deck.giveCard());
+  }
 
+  // play a card into another pile
+  playCard (card, discard_pile){
+    discard_pile.receiveCard(this.hand.giveCard(card));
+  }
 
 }
 
-class Deck {
+
+// A card object contains the passed-in attributes of a specific card
+class Card {
+  constructor(factionValue, rankValue, idValue) {
+    this.currentFaction = factionValue;
+    this.currentRank = rankValue;
+    this.currentId = idValue;
+  }
+
+  // getter/setter pairs for object properties.
+  get rank() { return this.currentRank; }
+  get faction() { return this.currentFaction; }
+  get id() { return this.currentId; }
+  
+  set rank(newRank) { this.rank = newRank; }
+  set faction(newFaction) { this.faction = newFaction; }
+  set id(newId) { this.currentId = newId; }
+
+  //provide a quick description of a card, in long or short form
+  description(short) {
+    if (short) {
+      return this.currentFaction[0] + this.currentRank;
+    }
+    return `Level ${this.currentRank} ${this.currentFaction} (ID: ${this.currentId})\n`
+  } 
+}
+
+// This is the parent class for all objects that are a collection of card objects
+class Collection {
+  constructor(cards) {
+    if (cards) {
+      this.cards = cards;
+    } else this.cards = [];
+  }
+
+  // Choose a random index within the boundaries of the collection
+  // and remove it from the array, then return that card
+  giveRandomCard() {
+    const randomIndex = Math.floor(Math.random()*this.cards.length);
+    const card = this.cards.splice(randomIndex, 1);
+    return card[0];
+  }
+
+  // Find a specific card in this collection, and remove it from
+  // this collection, and return that cars
+  giveCard(card) {
+    index = this.cards.findIndex(card);
+    if (index > -1) {
+      const newCard = this.cards.splice(index, 1);
+      console.log("Found card!");
+      return newCard[0];
+    } else {
+      return undefined;
+    }
+  }
+
+  // Add a passed card object to this collection
+  receiveCard(card) {
+    this.cards.push(card);
+  }
+
+  // Return a list of every card in this collection
+  getCollection(verbosity) {
+    let collectionList = ``;
+    for (let item of this.cards) {
+      collectionList += `${item.description(verbosity)}`;
+    }
+    return collectionList;
+  }
+
+}
+
+// A deck is a type of collection, but constructs a new set of cards 
+// of a passed-in array of faction types
+class Deck extends Collection {
   constructor(factions) {
     const newDeck = [];
     for (let rankIndex = 1; rankIndex < 6; rankIndex++) {
@@ -15,41 +99,20 @@ class Deck {
     }
     this.currentDeck = newDeck;
   }
-
-  get deck() {
-    return this.currentDeck;
-  }
-
-  pullCard() {
-    const randomIndex = Math.floor(Math.random()*this.currentDeck.length);
-    const card = this.currentDeck.splice(randomIndex, 1);
-    return card;
-  }
-
-
 }
 
-class Card {
-  constructor(faction, rank, id) {
-    this.current_faction = faction;
-    this.current_rank = rank;
-    this.id = id;
-  }
-
-  get rank() {
-    return this.current_rank;
-  }
-  
-  get faction() {
-    return this.current_faction;
-  }
-
-  set rank(newRank) {
-    this.current_rank = newRank;
-  }
-
-  set faction(newFaction) {
-    this.current_faction = newFaction;
+// A hand is a type of collection that lives inside a player object
+// and takes 5 random cards from a deck when instantiated.
+class Hand extends Collection {
+  constructor(deck) {
+    // inherited contructor makes empty hand
+    super(); 
+    // draw 5 random card from the deck
+    this.receiveCard(deck.giveRandomCard());
+    this.receiveCard(deck.giveRandomCard());
+    this.receiveCard(deck.giveRandomCard());
+    this.receiveCard(deck.giveRandomCard());
+    this.receiveCard(deck.giveRandomCard());  
   }
 }
 
