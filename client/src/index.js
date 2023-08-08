3db2f3af5ce825528a688d7ab4784f3423f777cf
Patrick Tumu <patricktumu@gmail.com>
2023-08-08 16:15:29 -0600

    trying to push again with Will

--- client/src/index.js
+++ client/src/index.js
@@ -22,7 +22,7 @@ import Phaser from 'phaser';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
