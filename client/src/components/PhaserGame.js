bc227e7988be081f8e29a652aa40687f2629cbac
Patrick Tumu <patricktumu@gmail.com>
2023-08-08 13:44:06 -0600

    phaser now in modal working but also has the same duplicate bug as the log in function

--- client/src/components/PhaserGame.js
+++ client/src/components/PhaserGame.js
@@ -6,13 +6,16 @@ const PhaserGame = () => {
 
   useEffect(() => {
     const config = {
-      width: 300,
-      height: 300
+      type: Phaser.AUTO,
+      parent: gameContainerRef.current,
+      width: 800, // Set the canvas width
+      height: 600, // Set the canvas height
+     
     };
     new Phaser.Game(config);
   }, []);
 
-  return <div ref={gameContainerRef} />;
+  return <div ref={gameContainerRef} style={{ width: '100%', height: '100%' }} />;
 };
 
 export default PhaserGame;
