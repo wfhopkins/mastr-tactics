6d8f4a1182bee514d46ae3fe89d9cf7b7e5105eb
Patrick Tumu <patricktumu@gmail.com>
2023-08-08 20:13:45 -0600

    did some organizing with images ans style folder. Made login look sweet

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,48 +1,54 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import LoginImage from '../images/login-page-image.png';
+import '../styles/Login.css';
 
 // Define the Login component
 export default function Login(props) {
   // State to manage email and password inputs
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   // Handle form submission
   const onSubmit = function(event) {
     event.preventDefault();
     // Call the login function from props if email is provided
-    email && props.login(email, password);
+    username && props.login(username, password);
   };
 
   // Render the Login component
   return (
     <div className="login">
-      {/* Login form */}
-      <form onSubmit={onSubmit}>
-        {/* Email input */}
-        <p>
-          <input
-            type="text"
-            name="username"
-            value={email}
-            placeholder="Enter Username or email"
-            onChange={event => setEmail(event.target.value)}
-          />
-        </p>
-        {/* Password input */}
-        <p>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            placeholder="Password"
-            onChange={event => setPassword(event.target.value)}
-          />
-        </p>
-        {/* Submit button */}
-        <p className="submit">
-          <button type="submit" name="commit">Login</button>
-        </p>
-      </form>
+      <div className="login-container">
+        <div className="background-image" style={{ backgroundImage: `url(${LoginImage})` }}>
+          {/* Login form */}
+          <form className="login-form" onSubmit={onSubmit}>
+            {/* Username input */}
+            <p>
+              <input
+                type="text"
+                name="username"
+                value={username}
+                placeholder="Enter Username"
+                onChange={event => setUsername(event.target.value)}
+              />
+            </p>
+            {/* Password input */}
+            <p>
+              <input
+                type="password"
+                name="password"
+                value={password}
+                placeholder="Password"
+                onChange={event => setPassword(event.target.value)}
+              />
+            </p>
+            {/* Submit button */}
+            <p className="submit">
+              <button type="submit" name="commit">Login</button>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
